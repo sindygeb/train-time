b8773c33d9f924615458acf3c457ba9590f63192
Sindy <sindy@vidku.com>
2019-01-28 01:48:11 -0600

    train finish
    train finish

--- assets/javascript/site.js
+++ assets/javascript/site.js
@@ -1,94 +1,110 @@
+$(document).ready(function(){
 
-// Initialize Firebase
-var config = {
-    apiKey: "AIzaSyDZ2zi8Zk5aLcwG_5pTmof4fJnINluJeqU",
-    authDomain: "sindy-train-time.firebaseapp.com",
-    databaseURL: "https://sindy-train-time.firebaseio.com",
-    projectId: "sindy-train-time",
-    storageBucket: "",
-    messagingSenderId: "1009508371692"
-  };
-  firebase.initializeApp(config);
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyDZ2zi8Zk5aLcwG_5pTmof4fJnINluJeqU",
+        authDomain: "sindy-train-time.firebaseapp.com",
+        databaseURL: "https://sindy-train-time.firebaseio.com",
+        projectId: "sindy-train-time",
+        storageBucket: "",
+        messagingSenderId: "1009508371692"
+    };
+    firebase.initializeApp(config);
 
-// Create a variable to reference the database.
-var database = firebase.database();
+    // Create a variable to reference the database.
+    var database = firebase.database();
 
-// button click event
-$("#add-train").on("click", function (event) {
-    event.preventDefault();
+    // button click event
+    $("#add-train").on("click", function (event) {
+        event.preventDefault();
 
-    trainName = $("#inputName").val().trim();
-    trainDes = $("#inputDes").val().trim();
-    trainFreq = $("#inputFreq").val().trim();
-    trainTime = $("#inputTime").val().trim();
+        var trainName = $("#inputName").val().trim();
+        var trainDes = $("#inputDes").val().trim();
+        var trainFreq = $("#inputFreq").val().trim();
+        var trainTime = $("#inputTime").val().trim();
 
 
-    database.ref().push({
-        Name: trainName,
-        Minutes: trainFreq,
-        Destination: trainDes,
-        Time: trainTime,
+        database.ref().push({
+            name: trainName,
+            minutes: trainFreq,
+            destination: trainDes,
+            time: trainTime,
+        });
+
+        $("#inputName").val("");
+        $("#inputDes").val("");
+        $("#inputFreq").val(0);
+
     });
 
-    $("#inputName").val("");
-    $("#inputDes").val("");
-    $("#inputFreq").val(0);
+    // Use "child-added" which creates a new object
+    database.ref().on("child_added", function(childSnapshot) {
 
+        //variables for the Snapshots
+        var trainName = childSnapshot.val().name;
+        var trainDes = childSnapshot.val().destination;
+        var trainFreq = childSnapshot.val().minutes;
+        var trainTime = childSnapshot.val().time;
+
+
+        // Log Childsnapshots for ref
+        console.log(childSnapshot.val());
+        console.log(trainName);
+        console.log(trainDes);
+        console.log(trainFreq);
+        console.log(trainTime);
+
+        //convert time for first train
+        var firstTrain = moment(trainTime, "HH:mm").subtract(1, "years");
+        
+        //current time (momentjs)
+        var currentTime = moment();
+
+        // Difference between the current and Time
+        var timeDiff = moment().diff(firstTrain, "minutes");
+        console.log(timeDiff);
+
+        var remainder = timeDiff % trainFreq;
+        console.log("remaining time: " + remainder);
+
+        var timeUntil = trainFreq - remainder;
+        console.log("time until next train: " + timeUntil);
+
+        var nextTrain = moment().add(timeUntil, "minutes");
+        var trainFormat = moment(nextTrain).format("HH:mm");
+
+        //couldn't figure out how to make it reset to the next time.
+        //var timeUntil = moment(firstTrain).fromNow();
+        //couldn't get this to work...
+        //var finalTime = moment.duration(timeNew).asMinutes();
+        //console.log(timeUntil);
+        //didn't work
+        //console.log(finalTime);
+
+
+        //it's not pretty, but it works...
+        $("#myTable").append(
+            "<tr>" + "<td>" + 
+            trainName + 
+            "</td>" + "<td>" + 
+            trainDes + 
+            "</td>" + "<td>" + 
+            trainFreq + 
+            "</td>" + "<td>" + 
+            trainTime +
+            "</td>" + "<td>" +
+            trainFormat +
+            "</td>" + "<td>" +
+            timeUntil +
+            "</td>" + "</tr>"
+            );
+
+        // Handle the errors
+    }, function(errorObject) {
+        console.log("Errors handled: " + errorObject.code);
+    });
+
+    function pushData() {
+
+    };
 });
-
-// Use "child-added" which creates a new object
-database.ref().on("child_added", function(snapshot) {
-
-    // Log snapshots for ref
-    console.log(snapshot.val());
-    console.log(snapshot.val().Name);
-    console.log(snapshot.val().Minutes);
-    console.log(snapshot.val().Destination);
-    console.log(snapshot.val().Time);
-
-    var orgTime = moment(snapshot.val().Time, "HH:mm").format("h:mm A");
-    var minAway;
-    // convert train time from military to standard time
-    var timeNew = moment(snapshot.val().Time, "h:mm A");
-    // Difference between the current and Time
-    var diffTime = moment().diff(moment(timeNew), "minutes");
-    var remainder = diffTime % snapshot.val().Minutes;
-    // Minutes until next train
-    var minAway = snapshot.val().Minutes - remainder;
-    // Next train time by adding minutes to the time
-    var nextTrain = moment().add(minAway, "minutes");
-    nextTrain = moment(nextTrain).format("h:mm A");
-
-    //couldn't figure out how to make it reset to the next time.
-    var timeUntil = moment(timeNew).fromNow("mm");
-    //couldn't get this to work...
-    //var finalTime = moment.duration(timeNew).asMinutes();
-    console.log(timeUntil);
-    //didn't work
-    //console.log(finalTime);
-
-
-    //it's not pretty, but it works...
-    $("#myTable").append(
-        "<tr>" + "<td>" + 
-        snapshot.val().Name + 
-        "</td>" + "<td>" + 
-        snapshot.val().Destination + 
-        "</td>" + "<td>" + 
-        snapshot.val().Minutes + 
-        "</td>" + "<td>" + 
-        orgTime +
-        "</td>" + "<td>" +
-        timeUntil +
-        "</td>" + "</tr>"
-        );
-
-
-    // Handle the errors
-  }, function(errorObject) {
-    console.log("Errors handled: " + errorObject.code);
-  });
-
-function pushData() {
-
-};
